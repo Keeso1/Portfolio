cdc1199eb7a0fa70bd19e64a4c92055fe8e5fcf3
Isac Lindh <isac.lindh01@gmail.com>
2025-08-02 00:08:02 +0200

    padding bug fix

--- project-card.js
+++ project-card.js
@@ -15,7 +15,7 @@ class MyComponent extends HTMLElement {
     flex-flow: column;
     width: 20rem;
     max-width: 25rem;
-    padding: 1rem;
+    margin: 1rem;
     aspect-ratio: 16 / 9;
     justify-content: flex-end;
     cursor: pointer;
